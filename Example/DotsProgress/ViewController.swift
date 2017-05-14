dba60a607f3e72a1af7e162f00efc6d35944ef28
makomori <makomori26@gmail.com>
2017-05-14 18:08:26 +0900

    Create project

--- Example/DotsProgress/ViewController.swift
+++ Example/DotsProgress/ViewController.swift
@@ -7,17 +7,37 @@
 //
 
 import UIKit
+import DotsProgress
 
 class ViewController: UIViewController {
 
+    var progressView: DotsProgressView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func showGoogleProgress(_ sender: Any) {
+        self.stop(self)
+        self.progressView = DotsProgressView(colors: nil)
+        self.progressView?.show()
+    }
+    
+    @IBAction func showCustomProgress(_ sender: Any) {
+        self.stop(self)
+        let firstColor = UIColor(red: 1.0, green: 0.631, blue: 0.761, alpha: 1.0)
+        let secondColor = UIColor(red: 1.0, green: 0.431, blue: 0.631, alpha: 1.0)
+        let thirdColor = UIColor(red: 1.0, green: 0.259, blue: 0.522, alpha: 1.0)
+        let fourthColor = UIColor(red: 1.0, green: 0.082, blue: 0.408, alpha: 1.0)
+        self.progressView = DotsProgressView(colors: [firstColor, secondColor, thirdColor, fourthColor])
+        self.progressView?.show()
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        if let _ = self.progressView {
+            self.progressView?.stop()
+            self.progressView = nil
+        }
     }
 
 }
---
+++ Example/Pods/Target Support Files/DotsProgress/DotsProgress-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DotsProgress : NSObject
+@end
+@implementation PodsDummy_DotsProgress
+@end
